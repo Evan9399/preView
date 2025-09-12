01404cd0c69677b8fe6513ff1c91e8561e1ce090
Evan9399 <kiritoku8299@gmail.com>
2025-09-12 21:47:28 +0800

    module、script、todoList

---
+++ math.js
@@ -0,0 +1,13 @@
+// math.js
+export const add = (a, b) => a + b;
+export const subtract = (a, b) => a - b;    
+const hello = (x)=> `hello ${x}`;
+// export default hello;
+
+const utils = {
+    add,
+    subtract,
+    hello
+}
+export default utils;
+
